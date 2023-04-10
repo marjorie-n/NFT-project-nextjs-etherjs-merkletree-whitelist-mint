60f9a3e4895c5ea52492091bb20ebf006f0a7643
Margotte83 <marjorie.ngoupende@gmail.com>
2023-04-10 10:16:52 +0200

    build: add config webpack & fs

--- front/next.config.js
+++ front/next.config.js
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// const nextConfig = {
+//   reactStrictMode: true,
+//   build: {
+//     extend(config, {}) {
+//         config.node = {
+//             fs: 'empty'
+//         }
+//     }
   
+//   },
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false
+        }
+    }
+    
+    return config;
+}
 }
 
+  
 
-
-module.exports = nextConfig
